16b6f7da34ee6246802d5db81581cb77d37a8c57
Rémi <remi.boivin@epitech.eu>
2021-07-28 18:23:24 +0200

    add cancancan

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
-  get 'about/', to: 'home#about'
-  get 'contact/', to: 'home#contact'
+  devise_for :users
+  root to: "home#index"
+  get "about/", to: "home#about"
+  get "contact/", to: "home#contact"
 end
